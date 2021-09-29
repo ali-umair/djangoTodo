b20b774eb84bc702d0594b5b512c46c9d9e5117e
ali-umair <17-CP-62@students.uettaxila.edu.pk>
2021-09-29 14:53:27 +0500

    added detail class view

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='index'),
+    path('', views.TaskList.as_view(), name='tasklist'),
+    path('task/<int:pk>', views.DetailList.as_view(), name='detaillist'),
 ]
