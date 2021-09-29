616fcd8e0ee42f822b52c127951045f9ea556e12
ali-umair <17-CP-62@students.uettaxila.edu.pk>
2021-09-29 14:29:46 +0500

    added class based view and its template

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def index(request):
-    return HttpResponse('All is Good, Proceed...')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'Tasks'
