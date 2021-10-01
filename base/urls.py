30de514ba72d6a54b0668d789aa4ab914aba83bf
ali-umair <17-CP-62@students.uettaxila.edu.pk>
2021-10-01 11:40:07 +0500

    Added create task functionality

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasklist'),
     path('task/<int:pk>', views.DetailList.as_view(), name='detaillist'),
+    path('create', views.TaskCreate.as_view(), name='create')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -14,3 +16,10 @@ class DetailList(DetailView):
     model = Task
     context_object_name = 'TasksDetail'
     template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'base/create_task.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasklist')
